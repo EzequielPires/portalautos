991392c8a0c510bdfb036bba6ddfb7015b7e0536
Roger Mesquita <rogermesquitaa@hotmail.com>
2022-03-02 17:15:41 -0300

    3.0 Commit

--- src/components/CarouselCard/index.tsx
+++ src/components/CarouselCard/index.tsx
@@ -2,16 +2,14 @@ import Carousel from 'react-bootstrap/Carousel'
 
 export function CarouselCard({array}) {
     return (
-        <Carousel fade interval={null}>
-            {array?.map(item => (
-                <Carousel.Item key={item.name}>
-                    <img
-                        src={"https://classificados.portalcatalao.com.br/veiculos/"
-                            + item.path}
-                        alt="First slide"
-                    />
-                </Carousel.Item>
-            ))}
+        <Carousel fade interval={null} touch={false}>
+            <Carousel.Item key={array[0].name}>
+                <img
+                    src={"https://classificados.portalcatalao.com.br/veiculos/"
+                        + array[0].path}
+                    alt="First slide"
+                />
+            </Carousel.Item>
         </Carousel>
     )
 }
