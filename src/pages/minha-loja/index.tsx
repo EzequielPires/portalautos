d51c3ec284989f3a2ea6529d4e670681ce4b42eb
EzequielPires <ezequiel.pires082000@gmail.com>
2022-04-07 16:56:48 -0300

    Commit

--- src/pages/minha-loja/index.tsx
+++ src/pages/minha-loja/index.tsx
@@ -1,9 +1,34 @@
 import Head from "next/head";
+import { useEffect, useState } from "react";
+import { FaImage } from "react-icons/fa";
+import { InputDefault } from "../../components/InputDefault";
 import { MenuAside } from "../../components/MenuAside";
 import { Navbar } from "../../components/Navbar";
+import { useFetchDefault } from "../../hooks/useFetchDefault";
+import useForm from "../../hooks/useForm";
 import styles from "./styles.module.scss";
 
 export default function MinhaLoja() {
+    const { fetch, value } = useFetchDefault();
+    const [store, setStore] = useState(null);
+    const name = useForm('name');
+    const phone = useForm('tel');
+    const wpp = useForm('tel');
+    const email = useForm('email');
+
+    const handleStore = async () => {
+        const res = await fetch('store/view');
+        setStore(res.data);
+        res.data.name && name.setValue(res.data.name);
+        res.data.email && email.setValue(res.data.email);
+        res.data.phone_number && phone.setValue(res.data.phone_number);
+        res.data.wpp_number && wpp.setValue(res.data.wpp_number);
+    }
+
+    useEffect(() => {
+        handleStore();
+    }, []);
+
     return (
         <div className={styles.minha_loja}>
             <Head>
@@ -15,6 +40,30 @@ export default function MinhaLoja() {
                 <Navbar />
                 <MenuAside />
             </header>
+            {
+                store &&
+                <div className={styles.content}>
+                    <div className={styles.content_header}>
+                        <div className={styles.banner}>
+                            {store.banner ? <img src={`https://portalautos.com.br/` + store.banner.path} alt="" /> : <FaImage style={{ color: '#fff' }} />}
+                        </div>
+                        <div className={styles.logo}>
+                            {store.logo ? <img src={`https://portalautos.com.br/` + store.logo.path} alt="" /> : <FaImage style={{ color: '#fff' }} />}
+                        </div>
+                        <div className="d-flex mt-3 mb-4 gap-2">
+                            <button className={styles.btn_add_image}>Adicionar Banner</button>
+                            <button className={styles.btn_add_image}>Adicionar Logo</button>
+                        </div>
+                    </div>
+                    <div className={styles.content_main}>
+                        <InputDefault type="text" id={'name'} label={"Nome da empresa"} {...name} />
+                        <InputDefault type="tel" id={'phone'} label={"Celular/telefone para contato"} {...phone} />
+                        <InputDefault type="tel" id={'wpp'} label={"Whatsapp para contato"} {...wpp} />
+                        <InputDefault type="email" id={'email'} label={"E-mail para contato"} {...email} />
+                        <button className={styles.btn_submit}>Salvar</button>
+                    </div>
+                </div>
+            }
         </div>
     )
 }
