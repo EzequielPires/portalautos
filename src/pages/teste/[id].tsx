33994cf174ceb6e30851b9e40dd03c581d0f0fee
EzequielPires <ezequiel.pires082000@gmail.com>
2022-05-04 17:41:59 -0300

    Commit Page test Roger

--- src/pages/teste/[id].tsx
+++ src/pages/teste/[id].tsx
@@ -1,14 +1,22 @@
 import axios from "axios"
 import { useEffect, useState } from "react";
+import {useRouter} from "next/router";
 
 export default function Teste() {
-    const [data, setData] = useState(null)
+    const [data, setData] = useState(null);
+    const router = useRouter();
+    const {id} = router.query
     const handleTeste = async () => {
-        await axios.get('https://api.portalautos.com.br/ad/22030710085/view-teset').then(res => setData(res.data));
+        console.log(router.asPath)
+        await axios.get('https://api.portalautos.com.br/ad/22030710085/view-teset', {
+            headers: {
+                'X-Requested-Uri': `${router.asPath}`
+            }
+        }).then(res => setData(res.data));
     }
     useEffect(() => {
-        handleTeste();
-    }, []);
+        id && handleTeste();
+    }, [id]);
     return (
         <div>
             <span>{data && JSON.stringify(data)}</span>
