c5faa9729b0abbf1093352a6172b493b1d58bc71
EzequielPires <ezequiel.pires082000@gmail.com>
2022-05-18 15:15:54 -0300

    Bug fix deploy

--- src/pages/teste/[id].tsx
+++ src/pages/teste/[id].tsx
@@ -1,38 +1,12 @@
-import axios from "axios"
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import {useRouter} from "next/router";
-import {api} from "../../services/api";
-import {parseCookies, setCookie} from "nookies";
-import {Session} from "../../entities/Session";
-import jwt from 'jsonwebtoken';
 
-export default function Teste({res}) {
-    const [data, setData] = useState(null);
+export default function Teste() {
     const router = useRouter();
-    const {id} = router.query
-    const handleTeste = async () => {
-        setData(res);
-
-    }
-    useEffect(() => {
-        res && handleTeste();
-    }, [res]);
+    const {id} = router.query;
     return (
         <div>
-            <span>{data && JSON.stringify(data)}</span>
+            <span>Id teste: {id}</span>
         </div>
     )
-}
-
-export async function getServerSideProps(context) {
-    let res = await api.get('https://api.portalautos.com.br/ad/22030710085/view-teset', {
-        headers: {
-            'X-Requested-Uri': `${context.resolvedUrl}`,
-        }
-    }).then(res => res.data).catch(error => {
-        console.log(error);
-    })
-    return {
-        props: {res}, // will be passed to the page component as props
-    }
 }
